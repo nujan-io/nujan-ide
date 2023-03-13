88658a14229d556ef4e37b4f971cfbb1a9137861
Rahul Yadav <rahulkyadav57@gmail.com>
2023-03-13 17:03:09 +0530

    Include test cases in template

--- src/constant/ProjectTemplate.ts
+++ src/constant/ProjectTemplate.ts
@@ -64,6 +64,99 @@ const contractChatBot = `;; Credit: https://github.com/LaDoger/doge.fc
   send_raw_message(msg, 3); ;; i was told that mode 64 is a better practice now, so (msg, 64)
 }`;
 
+const testCaseCounter = `// CONTRACT_BOC will be automatically updated with the build output at run time
+const {
+	core: { describe, it, expect, run, beforeEach },
+	prettify,
+} = window.jestLite;
+const Sandbox = window.Sandbox;
+const { Blockchain } = Sandbox;
+const TonCore = window.TonCore;
+const { beginCell, contractAddress, toNano } = TonCore;
+const Buffer = window.Buffer;
+
+class Counter {
+	address;
+	init;
+	static createForDeploy(code, initialCounterValue) {
+		const data = beginCell().storeUint(initialCounterValue, 64).endCell();
+		const workchain = 0;
+		const address = contractAddress(workchain, { code, data });
+		return new Counter(address, { code, data });
+	}
+
+	constructor(address, init) {
+		this.address = address;
+		this.init = init;
+	}
+
+	async sendDeploy(provider, via) {
+		await provider.internal(via, {
+			value: "0.01", // send 0.01 TON to contract for rent
+			bounce: false,
+		});
+	}
+
+	async getCounter(provider) {
+		const { stack } = await provider.get("counter", []);
+		return stack.readBigNumber();
+	}
+
+	async sendIncrement(provider, via) {
+		const messageBody = TonCore.beginCell()
+			.storeUint(1, 32) // op (op #1 = increment)
+			.storeUint(0, 64) // query id
+			.endCell();
+		await provider.internal(via, {
+			value: "0.002", // send 0.002 TON for gas
+			body: messageBody,
+		});
+	}
+}
+
+describe("Counter tests", async () => {
+	let blockchain;
+	let wallet1;
+	let counterContract;
+
+	beforeEach(async () => {
+		const counterCode = TonCore.Cell.fromBoc(
+			Buffer.from(
+				"{CONTRACT_BOC}",
+				"base64"
+			)
+		)[0];
+		const initialCounterValue = 20;
+		const counter = Counter.createForDeploy(counterCode, initialCounterValue);
+
+		blockchain = await Blockchain.create();
+		wallet1 = await blockchain.treasury("user1");
+		// deploy counter
+		counterContract = blockchain.openContract(counter);
+		await counterContract.sendDeploy(wallet1.getSender());
+	});
+
+	it("should get counter value", async () => {
+		const value = await counterContract.getCounter();
+		expect(value).toEqual(20n);
+	});
+
+	it("should increment the counter value", async () => {
+		await counterContract.sendIncrement(wallet1.getSender());
+		const counterValue = await counterContract.getCounter();
+		expect(counterValue).toEqual(21n);
+	});
+
+	it("should send ton coin to the contract", async () => {
+		console.log("sending 7.123 TON");
+		await wallet1.send({
+			to: counterContract.address,
+			value: toNano("7.123"),
+		});
+	});
+});
+`;
+
 export const ProjectTemplate = {
   tonBlank: {
     func: [
@@ -114,6 +207,15 @@ cell.bits.writeUint(0, 64);`,
         content: '',
         disableActions: true,
       },
+      {
+        id: '6',
+        name: 'test.spec.js',
+        parent: null,
+        type: 'file' as const,
+        path: 'test.spec.js',
+        content: testCaseCounter,
+        disableActions: true,
+      },
     ],
   },
   tonCounter: {
@@ -147,6 +249,15 @@ cell.bits.writeUint(1, 32);
 cell.bits.writeUint(0, 64);`,
         disableActions: true,
       },
+      {
+        id: '4',
+        name: 'test.spec.js',
+        parent: null,
+        type: 'file' as const,
+        path: 'test.spec.js',
+        content: testCaseCounter,
+        disableActions: true,
+      },
     ],
   },
   chatBot: {
@@ -178,6 +289,15 @@ cell.bits.writeUint(34, 64);`,
         content: `let cell = new tonweb.boc.Cell;
 cell.bits.writeUint(1, 32);
 cell.bits.writeUint(0, 64);`,
+        disableActions: true,
+      },
+      {
+        id: '4',
+        name: 'test.spec.js',
+        parent: null,
+        type: 'file' as const,
+        path: 'test.spec.js',
+        content: testCaseCounter,
         disableActions: true,
       },
     ],
