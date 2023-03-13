92f82f726674cdec179bb6aaacef94d69547617a
Rahul Yadav <rahulkyadav57@gmail.com>
2023-03-13 21:29:08 +0530

    Resolve fs issue for browser

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
