b004ba56d831aea5e39ff57668f37e6cdb667924
Rahul Yadav <rahulkyadav57@gmail.com>
2023-07-23 00:55:14 +0530

    Reformat template code

--- src/constant/template/BlankContract.ts
+++ src/constant/template/BlankContract.ts
@@ -1,68 +1,67 @@
 export const BlankContract = {
   testScript: `import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
 
-  export type BlankContractConfig = {};
-  
-  export function blankContractConfigToCell(config: BlankContractConfig): Cell {
-      return beginCell().endCell();
-  }
-  
-  export class BlankContract implements Contract {
-      constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
-  
-      static createFromAddress(address: Address) {
-          return new BlankContract(address);
-      }
-  
-      static createFromConfig(config: BlankContractConfig, code: Cell, workchain = 0) {
-          const data = blankContractConfigToCell(config);
-          const init = { code, data };
-          return new BlankContract(contractAddress(workchain, init), init);
-      }
-  
-      async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
-          await provider.internal(via, {
-              value,
-              sendMode: SendMode.PAY_GAS_SEPARATELY,
-              body: beginCell().endCell(),
-          });
-      }
-  }`,
+export type BlankContractConfig = {};
+
+export function blankContractConfigToCell(config: BlankContractConfig): Cell {
+    return beginCell().endCell();
+}
+
+export class BlankContract implements Contract {
+    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+
+    static createFromAddress(address: Address) {
+        return new BlankContract(address);
+    }
+
+    static createFromConfig(config: BlankContractConfig, code: Cell, workchain = 0) {
+        const data = blankContractConfigToCell(config);
+        const init = { code, data };
+        return new BlankContract(contractAddress(workchain, init), init);
+    }
+
+    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().endCell(),
+        });
+    }
+}`,
   testCases: `import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-  import { Cell, toNano } from 'ton-core';
-  import { BlankContract } from './BlankContract';
-  
-  describe('BlankContract', () => {
-      let code: Cell;
-  
-      beforeAll(async () => {
-          code = await compile('main.fc');
-      });
-  
-      let blockchain: Blockchain;
-      let blankContract: SandboxContract<BlankContract>;
-  
-      beforeEach(async () => {
-          blockchain = await Blockchain.create();
-  
-          blankContract = blockchain.openContract(BlankContract.createFromConfig({}, code));
-  
-          const deployer = await blockchain.treasury('deployer');
-  
-          const deployResult = await blankContract.sendDeploy(deployer.getSender(), toNano('0.05'));
-  
-          expect(deployResult.transactions).toHaveTransaction({
-              from: deployer.address,
-              to: blankContract.address,
-              deploy: true,
-              success: true,
-          });
-      });
-  
-      it('should deploy', async () => {
-          // the check is done inside beforeEach
-          // blockchain and blankContract are ready to use
-      });
-  });
-    `,
+import { Cell, toNano } from 'ton-core';
+import { BlankContract } from './BlankContract';
+
+describe('BlankContract', () => {
+    let code: Cell;
+
+    beforeAll(async () => {
+        code = await compile('main.fc');
+    });
+
+    let blockchain: Blockchain;
+    let blankContract: SandboxContract<BlankContract>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+
+        blankContract = blockchain.openContract(BlankContract.createFromConfig({}, code));
+
+        const deployer = await blockchain.treasury('deployer');
+
+        const deployResult = await blankContract.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: blankContract.address,
+            deploy: true,
+            success: true,
+        });
+    });
+
+    it('should deploy', async () => {
+        // the check is done inside beforeEach
+        // blockchain and blankContract are ready to use
+    });
+});`,
 };
