ce0b325e4e741e2126150af9f847b6c956f06af3
Rahul Yadav <rahulkyadav57@gmail.com>
2023-11-10 03:49:46 +0400

    Add test cases for tact template

--- src/constant/template/BlankContract.ts
+++ src/constant/template/BlankContract.ts
@@ -64,4 +64,45 @@ describe('BlankContract', () => {
         // blockchain and blankContract are ready to use
     });
 });`,
+  tactTestCase: `import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import { toNano } from 'ton-core';
+import { BlankContract } from '../dist/tact_BlankContract';
+import '@ton-community/test-utils';
+
+describe('BlankContract', () => {
+let blockchain: Blockchain;
+let blankContract: SandboxContract<BlankContract>;
+
+beforeEach(async () => {
+    blockchain = await Blockchain.create();
+
+    blankContract = blockchain.openContract(await BlankContract.fromInit());
+
+    const deployer = await blockchain.treasury('deployer');
+
+    const deployResult = await blankContract.send(
+        deployer.getSender(),
+        {
+            value: toNano('0.05'),
+        },
+        {
+            $$type: 'Deploy',
+            queryId: 0n,
+        }
+    );
+
+    expect(deployResult.transactions).toHaveTransaction({
+        from: deployer.address,
+        to: blankContract.address,
+        deploy: true,
+        success: true,
+    });
+});
+
+it('should deploy', async () => {
+    // the check is done inside beforeEach
+    // blockchain and blankContract are ready to use
+});
+});
+`,
 };
--- src/constant/template/CounterContract.ts
+++ src/constant/template/CounterContract.ts
@@ -132,4 +132,86 @@ describe('CounterContract', () => {
     });
 });
 `,
+  tactTestCases: `import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import { toNano } from 'ton-core';
+import { TactCounter } from '../dist/tact_TactCounter';
+import '@ton-community/test-utils';
+
+describe('TactCounter', () => {
+    let blockchain: Blockchain;
+    let tactCounter: SandboxContract<TactCounter>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+
+        tactCounter = blockchain.openContract(await TactCounter.fromInit(0n));
+
+        const deployer = await blockchain.treasury('deployer');
+
+        const deployResult = await tactCounter.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'Deploy',
+                queryId: 0n,
+            }
+        );
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: tactCounter.address,
+            deploy: true,
+            success: true,
+        });
+    });
+
+    it('should deploy', async () => {
+        // the check is done inside beforeEach
+        // blockchain and tactCounter are ready to use
+    });
+
+    it('should increase counter', async () => {
+        const increaseTimes = 3;
+        for (let i = 0; i < increaseTimes; i++) {
+            console.log('increase'+ (i + 1/increaseTimes));
+
+            const increaser = await blockchain.treasury('increaser' + i);
+
+            const counterBefore = await tactCounter.getCounter();
+
+            console.log('counter before increasing', counterBefore);
+
+            const increaseBy = BigInt(Math.floor(Math.random() * 100));
+
+            console.log('increasing by', increaseBy);
+
+            const increaseResult = await tactCounter.send(
+                increaser.getSender(),
+                {
+                    value: toNano('0.05'),
+                },
+                {
+                    $$type: 'Add',
+                    queryId: 0n,
+                    amount: increaseBy,
+                }
+            );
+
+            expect(increaseResult.transactions).toHaveTransaction({
+                from: increaser.address,
+                to: tactCounter.address,
+                success: true,
+            });
+
+            const counterAfter = await tactCounter.getCounter();
+
+            console.log('counter after increasing', counterAfter);
+
+            expect(counterAfter).toBe(counterBefore + increaseBy);
+        }
+    });
+});  
+`,
 };
