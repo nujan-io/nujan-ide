0ff86c3118e874f7d68f0ae47e155b90a2661978
Rahul Yadav <rahulkyadav57@gmail.com>
2024-08-13 23:02:58 +0530

    chore: update @tact-lang/compiler to version 1.4.2

--- src/utility/tactLogger.ts
+++ src/utility/tactLogger.ts
@@ -1,59 +1,42 @@
-// import { LogLevel, LogMethods, Logger } from '@tact-lang/compiler';
-import { TactLogger as TactLoggerType } from '@tact-lang/compiler';
+import { ILogger, Logger, LogLevel } from '@tact-lang/compiler';
 import EventEmitter from './eventEmitter';
 
-// const logLevelToMethodName: { [key in LogLevel]: keyof LogMethods | null } = {
-//   [LogLevel.NONE]: null,
-//   [LogLevel.ERROR]: 'error',
-//   [LogLevel.WARN]: 'warn',
-//   [LogLevel.INFO]: 'info',
-//   [LogLevel.DEBUG]: 'debug',
-// };
-
-// function getLoggingMethod(level: LogLevel): keyof LogMethods | null {
-//   return logLevelToMethodName[level];
-// }
-
-// export default class TactLogger extends Logger {
-//   private levelLevel: LogLevel;
-//   constructor(level: LogLevel = LogLevel.INFO) {
-//     super(level);
-//     this.levelLevel = level;
-//   }
-//   protected log(level: LogLevel, message: string | Error): void {
-//     if (this.levelLevel === LogLevel.NONE) {
-//       return;
-//     }
-
-//     message = message instanceof Error ? message.message : message;
-
-//     if (level > this.levelLevel) return;
-//     const loggingMethod = getLoggingMethod(level);
-//     if (!loggingMethod) return;
-
-//     EventEmitter.emit('LOG', {
-//       text: message,
-//       type: loggingMethod as Exclude<keyof LogMethods, 'debug' | 'warn'>,
-//       timestamp: new Date().toISOString(),
-//     });
-//   }
-// }
-
-const TactLogger: TactLoggerType = {
-  log: (message: string) => {
-    printLog(message, 'info');
-  },
-  error: (message: string) => {
-    printLog(message, 'error');
-  },
+const logLevelToMethodName: { [key in LogLevel]: keyof ILogger | null } = {
+  [LogLevel.NONE]: null,
+  [LogLevel.ERROR]: 'error',
+  [LogLevel.WARN]: 'warn',
+  [LogLevel.INFO]: 'info',
+  [LogLevel.DEBUG]: 'debug',
 };
 
-const printLog = (message: string, type: 'info' | 'error') => {
-  EventEmitter.emit('LOG', {
-    text: message,
-    type: type,
-    timestamp: new Date().toISOString(),
-  });
-};
+function getLoggingMethod(level: LogLevel): keyof ILogger | null {
+  return logLevelToMethodName[level];
+}
 
-export default TactLogger;
+export default class TactLogger extends Logger {
+  private levelLevel: LogLevel;
+  constructor(level: LogLevel = LogLevel.INFO) {
+    super(level);
+    this.levelLevel = level;
+  }
+  protected log(level: LogLevel, message: string | Error): void {
+    if (this.levelLevel === LogLevel.NONE) {
+      return;
+    }
+
+    message = message instanceof Error ? message.message : message;
+
+    if (level > this.levelLevel) return;
+    const loggingMethod = getLoggingMethod(level);
+    if (!loggingMethod) return;
+
+    EventEmitter.emit('LOG', {
+      text: message,
+      type:
+        loggingMethod === 'debug'
+          ? 'warning'
+          : (loggingMethod as Exclude<keyof ILogger, 'debug' | 'warn'>),
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
