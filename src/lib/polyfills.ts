baa80c627e081ae2a0e88c8faf02eb6f1d7262fb
Rahul Yadav <rahulkyadav57@gmail.com>
2024-10-08 09:13:09 +0530

    fix: build error

--- src/lib/polyfills.ts
+++ src/lib/polyfills.ts
@@ -1,14 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-condition, @typescript-eslint/no-explicit-any */
+
 // Extend the Set prototype to include isSubsetOf
 interface Set<T> {
   isSubsetOf(otherSet: Set<T>): boolean;
 }
 
-if (!Set.prototype.isSubsetOf) {
-  Set.prototype.isSubsetOf = function <T>(
+if (!(Set.prototype as any).isSubsetOf) {
+  (Set.prototype as any).isSubsetOf = function <T>(
     this: Set<T>,
     otherSet: Set<T>,
   ): boolean {
-    for (let elem of this) {
+    // @ts-expect-error - TS doesn't know that this is a Set
+    for (const elem of this) {
+      // @ts-expect-error - TS doesn't know that otherSet is a Set
       if (!otherSet.has(elem)) {
         return false;
       }
@@ -16,3 +20,8 @@ if (!Set.prototype.isSubsetOf) {
     return true;
   };
 }
+
+/* eslint-enable */
+
+// Add an empty export statement to ensure it's treated as a module
+export {};
